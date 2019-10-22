ec3b659e2d2fbfa8290e5b48d3f16a998b562262
Caleb King <caleb.king536@gmail.com>
2019-10-22 07:35:31 -0600

    added functions daysInMonth and rockPaperScissors

--- Drills-2.js
+++ Drills-2.js
@@ -1,12 +1,14 @@
 'use strict';
 
 //Jedi Name
+
 function jediName(firstName, lastName) {
   return lastName.slice(0, 3) + firstName.slice(0, 2);
 }
 
 
 //To infinity...
+
 function beyond(num) {
   if(!isFinite(num)) {
     console.log('And beyond');
@@ -21,6 +23,7 @@ function beyond(num) {
 
 
 //Cracking the code
+
 function decode(encodedWord) {
   switch(encodedWord[0]) {
   case 'a':
@@ -36,7 +39,68 @@ function decode(encodedWord) {
   }
 }
 
-// const code1 = decode('daniel');
-// console.log(code1); 
-
 //DECODED MESSAGE = 'for loop'
+
+
+//How many days in a month
+
+function daysInMonth(month, leapYear = false) {
+  switch(month) {
+  case 'February':
+    return (leapYear ? 29 : 28);
+  case 'April':
+  case 'June':
+  case 'September':
+  case 'November':
+    return 30;
+  case 'January':
+  case 'March':
+  case 'May':
+  case 'July':
+  case 'August':
+  case 'October':
+  case 'December':
+    return 31;
+  default:
+    throw 'Must provide a valid month';
+  }
+}
+
+// const daysInMonth1 = daysInMonth('March', true);
+// console.log(daysInMonth1);
+
+
+//Rock Paper Scissors
+function rockPaperScissors(playerChoice) {
+  let computerChoice = Math.floor(Math.random() * 3) + 1;
+  let playerWon;
+
+  if ((typeof playerChoice !== 'number') || playerChoice < 1 || playerChoice > 3) {
+    throw 'Not a valid entry. Try 1, 2, or 3.';
+  }
+
+  if (playerChoice === computerChoice) {
+    return 'You Tied';
+  }
+
+  switch (playerChoice){
+  case 1:
+    computerChoice === 2 ? playerWon = false : playerWon = true;
+    break;
+  case 2:
+    computerChoice === 3 ? playerWon = false : playerWon = true;
+    break;
+  case 3:
+    computerChoice === 1 ? playerWon = false : playerWon = true;
+    break;
+  }
+
+  //return winner
+  if (playerWon) {
+    console.log('Congrats! You beat the computer!');
+  } else {
+    console.log('You lost to the computer... Drats!');
+  }
+}
+
+rockPaperScissors(2);
