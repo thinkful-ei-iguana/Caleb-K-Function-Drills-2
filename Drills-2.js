ec64303809eb1bc59a1779dcd72c6f6fce38aa94
Caleb King <caleb.king536@gmail.com>
2019-10-22 05:54:52 -0600

    Added functions beyond and decode

--- Drills-2.js
+++ Drills-2.js
@@ -1,8 +1,42 @@
 'use strict';
 
+//Jedi Name
 function jediName(firstName, lastName) {
   return lastName.slice(0, 3) + firstName.slice(0, 2);
 }
 
-const jediName1 = jediName('Caleb', 'King');
-console.log(jediName1);
+
+//To infinity...
+function beyond(num) {
+  if(!isFinite(num)) {
+    console.log('And beyond');
+  } else if (num > 0) {
+    console.log('To infinity');
+  } else if (num < 0) {
+    console.log('To negative infinity');
+  } else if (num === 0) {
+    console.log('Staying home');
+  }
+}
+
+
+//Cracking the code
+function decode(encodedWord) {
+  switch(encodedWord[0]) {
+  case 'a':
+    return encodedWord[1];
+  case 'b':
+    return encodedWord[2];
+  case 'c':
+    return encodedWord[3];
+  case 'd':
+    return encodedWord[4];
+  default:
+    return ' ';
+  }
+}
+
+// const code1 = decode('daniel');
+// console.log(code1); 
+
+//DECODED MESSAGE = 'for loop'
